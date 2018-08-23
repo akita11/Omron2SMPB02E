28dd9592e61f1c8d80139d444df2c32a2a11a8be
akita11 <akita@ifdl.jp>
2018-08-23 17:29:16 +0900

    modified test.ino

--- examples/test/test.ino
+++ examples/test/test.ino
@@ -7,14 +7,14 @@ void setup() {
   prs.begin();
   Serial.begin(9600);
   prs.set_mode(MODE_NORMAL);
-  delay(1000);
+  delay(300);
+}
+
+void loop() {
   float tmp = prs.read_temp();
   Serial.println("temperature [degC]");
   Serial.println(tmp);
   float pressure = prs.read_pressure();
   Serial.println("pressure [Pa]");
   Serial.println(pressure);
-}
-
-void loop() {
 }
